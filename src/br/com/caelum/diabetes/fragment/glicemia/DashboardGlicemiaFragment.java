de5835056882b070ef3849f517dbdbf97a836302
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-07-09 14:32:48 -0300

    arrumando design da tela do dashboar de glicemia

--- src/br/com/caelum/diabetes/fragment/glicemia/DashboardGlicemiaFragment.java
+++ src/br/com/caelum/diabetes/fragment/glicemia/DashboardGlicemiaFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import br.com.caelum.diabetes.R;
 import br.com.caelum.diabetes.calculos.CalculaMediaGlicemia;
 
@@ -41,14 +40,15 @@ public class DashboardGlicemiaFragment extends Fragment{
 		
 		CalculaMediaGlicemia medias = new CalculaMediaGlicemia(getActivity());
 		
-		EditText glicemiasHoje = (EditText) view.findViewById(R.id.media_glicemia_hoje);
+		Button glicemiasHoje = (Button) view.findViewById(R.id.media_glicemia_hoje);
 		glicemiasHoje.setText(medias.getMediaDoDia() + "");
 		
-		EditText glicemiasSemana = (EditText) view.findViewById(R.id.media_glicemia_semana);
+		Button glicemiasSemana = (Button) view.findViewById(R.id.media_glicemia_semana);
 		glicemiasSemana.setText(medias.getMediaDaSemana() + "");
 		
-		EditText glicemiasMes = (EditText) view.findViewById(R.id.media_glicemia_mes);
+		Button glicemiasMes = (Button) view.findViewById(R.id.media_glicemia_mes);
 		glicemiasMes.setText(medias.getMediaDoMes() + "");
+		
 		return view;
 	}
 }
