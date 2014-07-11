4089c8aedf1d08097b5e884f1c55038399fdc42e
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-07-11 01:44:00 -0300

    possibilitando deletar cada um dos itens das listagens

--- src/br/com/caelum/diabetes/fragment/glicemia/ListaGlicemiaFragment.java
+++ src/br/com/caelum/diabetes/fragment/glicemia/ListaGlicemiaFragment.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import br.com.caelum.diabetes.R;
@@ -15,22 +21,63 @@ import br.com.caelum.diabetes.dao.GlicemiaDao;
 import br.com.caelum.diabetes.model.Glicemia;
 
 public class ListaGlicemiaFragment extends Fragment{
+	private ListView listaGlicemias;
+	protected Glicemia glicemiaSelecionada;
+	private ArrayAdapter<Glicemia> adapter;
+	private List<Glicemia> glicemias;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.lista_glicemia, null);
 		
+		listaGlicemias = (ListView) view.findViewById(R.id.glicemias);
+		
+		listaGlicemias.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+				glicemiaSelecionada = (Glicemia) listaGlicemias.getItemAtPosition(position);
+				return false;
+			}
+		});
+		
+		registerForContextMenu(listaGlicemias);
+		
+		return view;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		carregaLista();
+	}
+	
+	private void carregaLista() {
 		DbHelper helper = new DbHelper(getActivity());
 		GlicemiaDao dao = new GlicemiaDao(helper);
 		
-		List<Glicemia> glicemias = dao.getGlicemias();
+		glicemias = dao.getGlicemias();
 		
 		helper.close();
 		
-		ArrayAdapter<Glicemia> adapter = new ArrayAdapter<Glicemia>(getActivity(), android.R.layout.simple_list_item_1, glicemias);
+		adapter = new ArrayAdapter<Glicemia>(getActivity(), android.R.layout.simple_list_item_1, glicemias);
 		
-		ListView listaGlicemias = (ListView) view.findViewById(R.id.glicemias);
-		listaGlicemias.setAdapter(adapter);
-		
-		return view;
+		listaGlicemias.setAdapter(adapter);		
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		MenuItem delete = menu.add("Deletar");
+		delete.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				DbHelper helper = new DbHelper(getActivity());
+				GlicemiaDao dao = new GlicemiaDao(helper);
+				dao.deletar(glicemiaSelecionada);
+				helper.close();
+				carregaLista();
+				
+				return false;
+			}
+		});
 	}
 }
