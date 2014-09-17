2ea60e88db0a0cc5b04fe733d08a527d5df65b3d
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-09-17 11:15:56 -0300

    começando com ormlite e mudando tabela de Paciente

--- src/br/com/caelum/diabetes/dao/DbHelper.java
+++ src/br/com/caelum/diabetes/dao/DbHelper.java
@@ -1,12 +1,20 @@
 package br.com.caelum.diabetes.dao;
 
+import java.sql.SQLException;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import br.com.caelum.diabetes.extras.TabelasBD;
 import br.com.caelum.diabetes.model.AlimentoFisico;
+import br.com.caelum.diabetes.model.DadosMedicos;
+import br.com.caelum.diabetes.model.Paciente;
+import br.com.caelum.diabetes.model.Refeicao;
 
-public class DbHelper extends SQLiteOpenHelper{
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class DbHelper extends OrmLiteSqliteOpenHelper{
 
 	private static final String DATABASE = "Diabetes";
 	
@@ -17,72 +25,61 @@ public class DbHelper extends SQLiteOpenHelper{
 	}
 
 	@Override
-	public void onCreate(SQLiteDatabase db) {
-		String refeicao = "CREATE TABLE " + TabelasBD.REFEICAO + " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"tipoRefeicao TEXT," + 
-				"data DATE);";
+	public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+		try {
+			TableUtils.createTable(connectionSource, Refeicao.class);
+			TableUtils.createTable(connectionSource, Paciente.class);
+			TableUtils.createTable(connectionSource, DadosMedicos.class);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 		
-		String alimentoFisico = "CREATE TABLE " + TabelasBD.ALIMENTO_FISICO + " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"nome TEXT UNIQUE NOT NULL, " +
-				"carboidrato DOUBLE, " +
-				"unidadeDeMedida TEXT);";
-		
-		String alimentoVirtual = "CREATE TABLE " + TabelasBD.ALIMENTO_VIRTUAL + " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"id_refeicao INTEGER, " +
-				"quantidade DOUBLE, " +
-				"id_alimento INTEGER," +
-				"FOREIGN KEY (id_alimento) references AlimentoFisico(id), " +
-				"FOREIGN KEY (id_refeicao) references Refeicao(id));";
-		
-		String paciente = "CREATE TABLE " + TabelasBD.PACIENTE + " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"nome TEXT, " +
-				"idade INTEGER, " +
-				"peso DOUBLE," +
-				"altura DOUBLE," +
-				"sexo TEXT," +
-				"tipoDiabetes TEXT);";
-		
-		String dadosMedicos = "CREATE TABLE " + TabelasBD.DADOS_MEDICOS+ " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"cafeDaManha DOUBLE, " +
-				"lancheDaManha DOUBLE, " +
-				"almoco DOUBLE, " +
-				"lancheDaTarde DOUBLE, " +
-				"jantar DOUBLE, " +
-				"ceia DOUBLE, " +
-				"tipoDado TEXT," +
-				"id_paciente INTEGER," + 
-				"FOREIGN KEY (id_paciente) references Paciente(id));";
-		
-		String glicemia = "CREATE TABLE " + TabelasBD.GLICEMIA + " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"tipoRefeicao TEXT," + 
-				"data DATE," + 
-				"valorGlicemia INTEGER);";
-		
-		String lembrete = "CREATE TABLE " + TabelasBD.LEMBRETE + " " + 
-				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-				"data DATE," + 
-				"atividade TEXT," + 
-				"anotacoes TEXT);";
-		
-		
-		db.execSQL(refeicao);
-		db.execSQL(alimentoFisico);
-		db.execSQL(alimentoVirtual);
-		db.execSQL(paciente);
-		db.execSQL(dadosMedicos);
-		db.execSQL(glicemia);
-		db.execSQL(lembrete);
-		insereAlimentoFisico(db);
+//		String alimentoFisico = "CREATE TABLE " + TabelasBD.ALIMENTO_FISICO + " " + 
+//				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//				"nome TEXT UNIQUE NOT NULL, " +
+//				"carboidrato DOUBLE, " +
+//				"unidadeDeMedida TEXT);";
+//		
+//		String alimentoVirtual = "CREATE TABLE " + TabelasBD.ALIMENTO_VIRTUAL + " " + 
+//				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//				"id_refeicao INTEGER, " +
+//				"quantidade DOUBLE, " +
+//				"id_alimento INTEGER," +
+//				"FOREIGN KEY (id_alimento) references AlimentoFisico(id), " +
+//				"FOREIGN KEY (id_refeicao) references Refeicao(id));";
+//		
+//		String glicemia = "CREATE TABLE " + TabelasBD.GLICEMIA + " " + 
+//				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//				"tipoRefeicao TEXT," + 
+//				"data DATE," + 
+//				"valorGlicemia INTEGER);";
+//		
+//		String lembrete = "CREATE TABLE " + TabelasBD.LEMBRETE + " " + 
+//				"(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//				"data DATE," + 
+//				"atividade TEXT," + 
+//				"anotacoes TEXT);";
+//		
+//		
+//		db.execSQL(alimentoFisico);
+//		db.execSQL(alimentoVirtual);
+//		db.execSQL(glicemia);
+//		db.execSQL(lembrete);
+//		insereAlimentoFisico(db);
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+		try {
+			TableUtils.dropTable(connectionSource, Paciente.class, true);
+			onCreate(db, connectionSource);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public <T> RuntimeExceptionDao<T, Integer> getSimpleDataDao(Class<T> clazz) {
+		return getRuntimeExceptionDao(clazz);
 	}
 	
 	public void insereAlimentoFisico(SQLiteDatabase db) {
--- src/br/com/caelum/diabetes/dao/PacienteDao.java
+++ src/br/com/caelum/diabetes/dao/PacienteDao.java
@@ -1,84 +1,37 @@
 package br.com.caelum.diabetes.dao;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-import br.com.caelum.diabetes.extras.Extras;
-import br.com.caelum.diabetes.extras.TabelasBD;
-import br.com.caelum.diabetes.model.DadosMedicos;
+import java.sql.SQLException;
+import java.util.List;
+
 import br.com.caelum.diabetes.model.Paciente;
-import br.com.caelum.diabetes.model.TipoDadoMedico;
+
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 
 public class PacienteDao {
-	private static final String TABELA = TabelasBD.PACIENTE;
-
-	private DbHelper helper;
+	private RuntimeExceptionDao<Paciente, Integer> dao;
 
 	public PacienteDao(DbHelper helper) {
-		this.helper = helper;
+		dao = helper.getSimpleDataDao(Paciente.class);
 	}
 	
-	public int salva(Paciente paciente) {
-		ContentValues values = toContentValues(paciente);
-		return (int) helper.getWritableDatabase().insert(TABELA, null, values);
+	public void salva(Paciente paciente) {
+		dao.create(paciente);
 	}
 
 	public void deletar(Paciente paciente) {
-		String[] args = {String.valueOf(paciente.getId())};
-		helper.getWritableDatabase().delete(TABELA, "id=?", args);
+		dao.delete(paciente);
 	}
 
 	public void atualiza(Paciente paciente) {
-		String[] args = {String.valueOf(paciente.getId())};
-		ContentValues values = toContentValues(paciente);
-		helper.getWritableDatabase().update(TABELA, values, "id=?", args);
+		dao.update(paciente);
 	}
 	
-	public Paciente getPaciente() {
-		Cursor cursor = helper.getReadableDatabase().rawQuery("SELECT * from " + TabelasBD.PACIENTE + ";", null);
-		if(cursor.moveToNext()) {
-			Paciente paciente = new Paciente();
-			paciente.setId(cursor.getInt(0));
-			paciente.setNome(cursor.getString(1));
-			paciente.setIdade(cursor.getInt(2));
-			paciente.setPeso(cursor.getDouble(3));
-			paciente.setAltura(cursor.getDouble(4));
-			paciente.setSexo(cursor.getString(5));
-			paciente.setTipoDiabetes(cursor.getString(6));
-			
-			paciente.setInsulinaCorrecao(getDadosMedicos(paciente, Extras.CORRECAO));
-			paciente.setInsulinaContinua(getDadosMedicos(paciente, Extras.CONTINUO));
-			paciente.setGlicemiaAlvo(getDadosMedicos(paciente, Extras.GLICEMIA_ALVO));
-			return paciente;
-		}
-		return null;
-	}
-
-	private DadosMedicos getDadosMedicos(Paciente paciente, String tipoDado) {
-		DadosMedicos dadosCorrecao = new DadosMedicos();
-		String[] args = {tipoDado};
-		Cursor cursor = helper.getReadableDatabase().rawQuery("SELECT * from " + TabelasBD.DADOS_MEDICOS+ " where tipoDado=?;", args);
-		if(cursor.moveToNext()) {
-			dadosCorrecao.setId(cursor.getInt(0));
-			dadosCorrecao.setCafeManha(cursor.getDouble(1));
-			dadosCorrecao.setLancheManha(cursor.getDouble(2));
-			dadosCorrecao.setAlmoco(cursor.getDouble(3));
-			dadosCorrecao.setLancheTarde(cursor.getDouble(4));
-			dadosCorrecao.setJantar(cursor.getDouble(5));
-			dadosCorrecao.setCeia(cursor.getDouble(6));
-			dadosCorrecao.setTipo(TipoDadoMedico.fromString(cursor.getString(7)));
-			dadosCorrecao.setPaciente(paciente);
-		}
-		return dadosCorrecao;
-	}
-	
-	private ContentValues toContentValues(Paciente paciente) {
-		ContentValues values = new ContentValues();
-		values.put("nome", paciente.getNome());
-		values.put("idade", paciente.getIdade());
-		values.put("peso", paciente.getPeso());
-		values.put("altura", paciente.getAltura());
-		values.put("tipoDiabetes", paciente.getTipoDiabetes());
-		values.put("sexo", paciente.getSexo());
-		return values;
+	public Paciente getPaciente() throws SQLException {
+		QueryBuilder<Paciente,Integer> builder = dao.queryBuilder();
+		PreparedQuery<Paciente> prepare = builder.prepare();
+		List<Paciente> list = dao.query(prepare);
+		return list.size() == 0 ? null : list.get(0);
 	}
 }
